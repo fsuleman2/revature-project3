fdbfa1b2a82b76775fe42c87a6b5776e248f1f35
sush-mi99 <susmitha.ccin008@gmail.com>
2021-09-08 21:29:38 +0530

    modified

--- trainfrontend/src/app/customer-login/customer-login.component.ts
+++ trainfrontend/src/app/customer-login/customer-login.component.ts
@@ -11,19 +11,4 @@ export class CustomerLoginComponent implements OnInit {
 
   ngOnInit(): void {
   }
-
-//show hide div variables
-userlogin = true;
-userregister = false;
-//Buttons clicks functionalities 
-user_register()
-{
-  this.userlogin = false;
-  this.userregister = true;
-}
-user_login()
-{
-  this.userlogin = true;
-  this.userregister = false;
-}
 }
