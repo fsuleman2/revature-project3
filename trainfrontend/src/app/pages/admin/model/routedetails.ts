1550d4c77e9045a4568c5cd1b39c05e9f4375898
Syed Suleman <46878182+fsuleman2@users.noreply.github.com>
2021-09-13 16:11:21 +0530

    changed route

--- trainfrontend/src/app/pages/admin/model/routedetails.ts
+++ trainfrontend/src/app/pages/admin/model/routedetails.ts
@@ -3,7 +3,8 @@ import { traindetails } from "./traindetails";
 export class routedetails{
     routeid:number=0;
 	startpoint:string='';
-	endpoint:string='';
+	gap:any=''; //transient
+	interval:any=''; //transien
 	date:any='';
 	arrival:any='';
 	depart:any='';
