658594e64408d2211ef6bf14bf98dca3b4dbd847
mohan30ard <mohantulabandhu@gmail.com>
2021-09-14 00:25:06 +0530

    added trains

--- trainfrontend/src/app/pages/admin/view-trains/view-trains.component.ts
+++ trainfrontend/src/app/pages/admin/view-trains/view-trains.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { TrainService } from 'src/app/services/train.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-view-trains',
@@ -6,10 +8,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./view-trains.component.css']
 })
 export class ViewTrainsComponent implements OnInit {
+  public trains = [{tid: '',
+    tno: '',
+    tname: '',
+    start: '',
+    stop: '',
+    date: '',
+    totalCoach: '',
+    acSleeperCoach: '',
+    acSittingCoach: '',
+    nonAcSleeperCoach: '',
+    nonAcSittingCoach: '',
+    totalAcSleeperSeat: "",
+    availAcSleeperSeat: "",
+    totalAcSittingSeat: "",
+    availAcSittingSeat: "",
+    totalNonAcSleeperSeat: "",
+    availNonAcSleeperSeat: "",
+    totalNonAcSittingSeat:"",
+    availNonAcSittingSeat: "",
+    category: {
+      catId: "",
+      type: "",
+      description: ""
+    }
+  }];
 
-  constructor() { }
+  constructor(private _train: TrainService) { }
 
   ngOnInit(): void {
+    this._train.Alltrains().subscribe(
+      (data: any) => {
+        this.trains = data;
+        console.log(this.trains);
+      },
+      (error:any) => {
+        console.log(error);
+        Swal.fire('Error !', 'Error in loading data !', 'error');
+      }
+    );
   }
 
 }
